23ce4d9ddb3a46f457be830fe606ad25eec087ed
Sean O'Flaherty <sean.oflaherty@mttnow.com>
2016-11-22 17:23:47 +0000

    Cleanup presenter.

--- app/src/main/java/com/mttnow/fluttr/PresenterActivity.java
+++ app/src/main/java/com/mttnow/fluttr/PresenterActivity.java
@@ -2,7 +2,6 @@ package com.mttnow.fluttr;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class PresenterActivity extends AppCompatActivity {
 
@@ -10,10 +9,6 @@ public class PresenterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_presenter);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
