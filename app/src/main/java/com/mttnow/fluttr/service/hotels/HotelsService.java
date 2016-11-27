97fbbeae551b24c15fc66c1f17b93543cef9208f
Sean O'Flaherty <sean.oflaherty@mttnow.com>
2016-11-27 13:46:07 +0000

    Move retrofit class to api, add intent for starting stream after search params are filled in.

--- app/src/main/java/com/mttnow/fluttr/service/hotels/HotelsService.java
+++ app/src/main/java/com/mttnow/fluttr/service/hotels/HotelsService.java
@@ -1,33 +1,19 @@
 package com.mttnow.fluttr.service.hotels;
 
-import android.content.Context;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.mttnow.fluttr.domain.hotels.Hotel;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+
 /**
- * Created by seanb on 22/11/2016.
+ * Created by seanof on 26/11/2016.
  */
 
-public class HotelsService {
+public interface HotelsService {
 
-  private Gson gson;
-
-   public HotelsService(Gson g) {
-    gson = g;
-  }
-
-  public List<Hotel> getHotels(String destination, Context c) throws IOException {
-    Reader reader = new BufferedReader(new InputStreamReader(c.getAssets().open("mock-expedia-hotels-" + destination + ".json"), "UTF-8"));
-    return gson.fromJson(reader, new TypeToken<ArrayList<Hotel>>() {}.getType());
-  }
-
+    @GET("/hotels")
+    Call<List<Hotel>> listHotels();
 }
